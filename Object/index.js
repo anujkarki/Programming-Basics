3efd33d4e875a44886f64cbd718386ee826eef4c
Anuj Karki <anujkarki553@gmail.com>
2022-02-21 20:42:49 +0545

    Object

---
+++ Object/index.js
@@ -0,0 +1,13 @@
+//object defination
+const person = {
+  firstName: 'Anuj',
+  lastName: 'Karki',
+  age: 22,
+  gender: 'Male',
+};
+
+//loging whole object
+console.log(person);
+
+//loging object properties
+console.log(person.firstName + ' ' + person['lastName']);
